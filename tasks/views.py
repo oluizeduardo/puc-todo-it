291c502f362748b08446a020394faf9587f04893
Luiz Eduardo da Costa <luiz_costa@hst.br>
2023-01-09 22:26:28 -0300

    Added admin configuration for Task

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
+
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    todos = Todo.objects.all()[:10]
+
+    context = {
+        'todos':todos
+    }
+
+    return render(request, 'tasks.html', context)
+
+def add(request):
+    if(request.method == 'POST'):
+        title = request.POST['title']
+        text = request.POST['text']
+
+        todo = Todo(title=title, text=text)
+        todo.save()
+
+        return redirect('/todos')
+    else:
+        return render(request, 'add.html')
